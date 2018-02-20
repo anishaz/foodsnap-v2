2f3e67ba0b25034b183b97814ebb9b5646cff082
AnishaZ <anishagzacharia@gmail.com>
2018-02-19 16:58:10 -0800

    Initial commit: Node, Express, React framework setup

---
+++ server/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Welcome to Express' });
+});
+
+module.exports = router;
