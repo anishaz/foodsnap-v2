e928e0327a9887e0245b53751e481a0fefcb377e
AnishaZ <anishagzacharia@gmail.com>
2018-02-26 20:16:58 -0800

    Removed form from logout button

--- public/src/components/button/button.jsx
+++ public/src/components/button/button.jsx
@@ -10,9 +10,14 @@ const Button = props => (
   <RaisedButton primary style={buttonStyles} type={props.type}>{ props.text }</RaisedButton>
 );
 
+Button.defaultProps = {
+  type: 'input',
+};
+
+
 Button.propTypes = {
   text: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  type: PropTypes.string,
 };
 
 
--- public/src/components/logout/logout.jsx
+++ public/src/components/logout/logout.jsx
@@ -1,51 +1,15 @@
 import React, { Component } from 'react';
-import client from '../../client'; // this is importing axios instance from client.js
 import Button from '../button/button';
 
+
 class Logout extends Component {
-    constructor() {
-        super();
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-        this.state = {
-            email: '',
-            password: '',
-            errors: [],
-        };
-    }
-
-    onChange = (e) => {
-        const state = this.state;
-        state[e.target.name] = e.target.value;
-        this.setState(state);
-    }
-
-    async onSubmit(e) {
-        let self = this;
-        e.preventDefault();
-        const { email, password } = this.state;
-        try {
-            let response = await client.post('/logout');
-            if (response) {
-                this.props.logOut();
-            }
-        }
-        catch (error) {
-            const errorRes = JSON.stringify(error.response.data);
-            console.log(JSON.stringify(error.response.data));
-            self.setState({ errors: errorRes });
-        }
-    }
-
-    render() {
-        return (
-            <div className="logout">
-                <form action="/" className="logout__form" onSubmit={this.onSubmit} >
-                    <Button text="Logout" type="submit" />
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="logout">
+        <button onClick={this.props.logOut}>Logout</button>
+      </div>
+    );
+  }
 }
 
 export default Logout;
